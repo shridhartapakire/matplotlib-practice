e5d19e0683e4d0869b755747ecd42a3ff4c123a1
Shridhar <shridhartapakire4051@gmail.com>
2026-04-17 23:21:03 +0530

    Matplotlib: basic line plot and setup

---
+++ matplotlib_basics.py
@@ -0,0 +1,13 @@
+# Matplotlib Day 1 - Basic Plotting
+
+import matplotlib.pyplot as plt
+
+# Data
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 40]
+
+# Line plot
+plt.plot(x, y)
+
+# Show graph
+plt.show()
