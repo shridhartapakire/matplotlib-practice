8d853bad0e4044ddf658c3cc0814bcc4bb79c751
shridhartapakire <shridhartapakire4051@gmail.com>
2026-04-18 23:16:48 +0530

    Matplotlib: added titles, labels, and styling to line plot

--- matplotlib_basics.py
+++ matplotlib_basics.py
@@ -1,13 +1,19 @@
-# Matplotlib Day 1 - Basic Plotting
-
 import matplotlib.pyplot as plt
 
 # Data
 x = [1, 2, 3, 4, 5]
 y = [10, 20, 25, 30, 40]
 
-# Line plot
-plt.plot(x, y)
+# Plot
+plt.plot(x, y, color='blue', linestyle='--', marker='o')
+
+# Title and labels
+plt.title("Sample Line Graph")
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+
+# Grid
+plt.grid(True)
 
 # Show graph
 plt.show()
