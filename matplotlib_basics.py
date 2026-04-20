e201f753ce39903feb452f72c1498e4757d3295a
Shridhar <shridhartapakire4051@gmail.com>
2026-04-20 12:05:51 +0530

    Matplotlib: added scatter plot for relationship visualization

--- matplotlib_basics.py
+++ matplotlib_basics.py
@@ -11,6 +11,8 @@ plt.xlabel("X Values")
 plt.ylabel("Y Values")
 plt.grid(True)
 
+plt.show()
+
 # -------- Bar Chart --------
 categories = ['Food', 'Travel', 'Shopping', 'Bills']
 amounts = [500, 300, 700, 400]
@@ -20,5 +22,20 @@ plt.bar(categories, amounts, color='green')
 plt.title("Expenses by Category")
 plt.xlabel("Category")
 plt.ylabel("Amount")
+
+plt.show()
+
+# -------- Scatter Plot --------
+
+x_scatter = [1, 2, 3, 4, 5]
+y_scatter = [5, 15, 20, 25, 30]
+
+plt.figure()
+
+plt.scatter(x_scatter, y_scatter, color='red')
+
+plt.title("Scatter Plot Example")
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
 
 plt.show()
